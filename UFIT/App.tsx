6b3ee83b00a8f8be70888547eabac167356a44ea
Kyle Grande <kyle.g.grande@gmail.com>
2023-06-22 11:45:30 -0500

    text changes

--- UFIT/App.tsx
+++ UFIT/App.tsx
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>THIS IS THE UFIT APP</Text>
+      <Text> It's gonna be a great app. </Text>
       <StatusBar style="auto" />
     </View>
   );
