8192c757b9ade68277565b7926e5fd2792e2971c
Ricardo Tlatelpa <ricardotlatelpa@Ricardos-MacBook-Pro.local>
2023-07-10 17:25:33 -0400

    added basic form to program creation page

--- UFIT/src/screens/UserProgramsStack/ProgramCreation/ProgramCreate.tsx
+++ UFIT/src/screens/UserProgramsStack/ProgramCreation/ProgramCreate.tsx
@@ -1,16 +1,51 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, SafeAreaView, TextInput } from "react-native";
 import {creatingStyles} from '../../style';
 
 export default function ProgramCreate() {
+    const [text, onChangeText] = React.useState('Placeholder Text');
+    const [number, onChangeNumber] = React.useState('');
     return (
         <View style={creatingStyles.viewContainer}>
-            <Text>
-                One day
-            </Text>
-            <Text>
-                You'll be able to create here
-            </Text>
+            <View style = {{paddingLeft:15}}>
+                <Text style={{fontSize:30,fontWeight:'bold',color:'white'}}>
+                    Create A Program
+                </Text>
+
+                <SafeAreaView>
+                    <Text style={{fontSize:20, color: 'white', fontWeight: 'bold'}}>
+                        Name:
+                    </Text>
+                    <TextInput
+                        style={{
+                            height: 40,
+                            margin: 12,
+                            borderWidth: 1,
+                            padding: 10,
+                            color: 'white',
+                            borderColor: 'white'
+                          }}
+                        onChangeText={onChangeText}
+                        value={text}
+                    />
+                    <Text style={{fontSize:20, color: 'white', fontWeight: 'bold'}}>
+                        Description:
+                    </Text>
+                    <TextInput
+                        style={{
+                            height: 40,
+                            margin: 12,
+                            borderWidth: 1,
+                            padding: 10,
+                            color: 'white',
+                            borderColor: 'white'
+                          }}
+                        onChangeText={onChangeNumber}
+                        value={number}                                                
+                    />
+                </SafeAreaView>
+            </View>
         </View>
     );
 }
+
