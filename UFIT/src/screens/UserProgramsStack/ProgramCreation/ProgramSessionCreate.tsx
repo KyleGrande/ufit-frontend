16a03074a0bbb3611bfb37fe9e714f5acc7e4f55
Ricardo Tlatelpa <ricardotlatelpa@Ricardos-MacBook-Pro.local>
2023-07-17 14:33:58 -0400

    Rest day picker added to session creator page

--- UFIT/src/screens/UserProgramsStack/ProgramCreation/ProgramSessionCreate.tsx
+++ UFIT/src/screens/UserProgramsStack/ProgramCreation/ProgramSessionCreate.tsx
@@ -13,7 +13,7 @@ import {
   NativeStackNavigationProp,
 } from "@react-navigation/native-stack";
 import { StackParamList } from "../../UserPrograms";
-
+import { Picker } from "@react-native-picker/picker";
 
 type ProgramsMainScreenProps = {
   navigation: NativeStackNavigationProp<StackParamList, "User Programs">;
@@ -23,12 +23,12 @@ export default function ProgramSessionCreate({
   navigation,
 }: ProgramsMainScreenProps) {
   const [sessionName, onChangeSessionName] = React.useState("");
-  
+  const [restdays, onChangeRestDay] = React.useState("0");
 
   return (
     <View style={creatingStyles.viewContainer}>
       <View style={{ paddingLeft: 15 }}>
-        <ScrollView>
+        <ScrollView style = {{height: '90%'}}>
           <Text style={{ fontSize: 30, fontWeight: "bold", color: "white" }}>
             Session
           </Text>
@@ -49,28 +49,55 @@ export default function ProgramSessionCreate({
               onChangeText={onChangeSessionName}
               value={sessionName}
             />
-         
+
+            <Text
+              style={{
+                fontSize: 20,
+                color: "white",
+                fontWeight: "bold",
+                marginTop: 40,
+              }}
+            >
+              Rest Days:
+              <Text style={{ flexDirection: "row", color: "red" }}>*</Text>
+            </Text>
+
+            <Picker
+              style={{ color: "white", marginTop: 0, paddingTop: 0 }}
+              selectedValue={restdays}
+              onValueChange={(itemValue: any) => onChangeRestDay(itemValue)}
+              itemStyle={{ color: "white", fontSize: 40 }}
+            >
+              <Picker.Item label="0" value="0" />
+              <Picker.Item label="1" value="1" />
+              <Picker.Item label="2" value="2" />
+              <Picker.Item label="3" value="3" />
+              <Picker.Item label="4" value="4" />
+              <Picker.Item label="5" value="5" />
+              <Picker.Item label="6" value="6" />
+            </Picker>
           </SafeAreaView>
+          <View>
+            <Button
+              title="Add Movement"
+              color="orange"
+              onPress={() => {
+                console.log("Adding Movement to program");
+                navigation.navigate("Create a Movement");
+              }}
+            />
+
+            <Button
+              title="Save Session"
+              color="orange"
+              onPress={() => {
+                console.log("Creating session");
+                navigation.navigate("Create a Program");
+              }}
+            />
+          </View>
         </ScrollView>
       </View>
-
-      <Button
-        title="Add Movement"
-        color="orange"
-        onPress={() => {
-          console.log("Adding Movement to program");
-          navigation.navigate("Create a Movement");
-        }}
-      />
-
-      <Button
-        title="Save Session"
-        color="orange"
-        onPress={() => {
-          console.log("Creating session");
-          navigation.navigate("Create a Program");
-        }}
-      />
     </View>
   );
 }
