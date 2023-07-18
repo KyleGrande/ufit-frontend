087041413a2fe13e8f083a5e7c8626f24da6a10f
Kyle Grande <kyle.g.grande@gmail.com>
2023-07-18 15:04:35 -0500

    style

--- UFIT/src/screens/ProgramFeed.tsx
+++ UFIT/src/screens/ProgramFeed.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import { Text, View, Pressable} from 'react-native';
-import { styles } from './style';
+import { Text, View, Pressable, ScrollView} from 'react-native';
+import { FeedStyles } from './style';
 import API, {Program} from '../api';
 
 export default function ProgramFeed() {
@@ -23,25 +23,34 @@ export default function ProgramFeed() {
     // if there is an error
     if (error) {
         return (
-            <View style={styles.viewContainer}>
-                <Text style={styles.titleBarText}>{error}</Text>
+            <View style={FeedStyles.viewContainer}>
+                <Text style={FeedStyles.titleBarText}>{error}</Text>
             </View>
         );
     }
 
     return (
         <View>
-            <View style={styles.viewContainer}>
-                <Text style={styles.titleBarText}>
+            <View style={FeedStyles.viewContainer}>
+                <Text style={FeedStyles.titleBarText}>
                     Discover
                 </Text>
-                <Text>
-                    {programs.map((program) => (
-                        <Text key={program._id}>
-                            {program.programName}
-                        </Text>
+                <ScrollView style={FeedStyles.programsContainer}// horizontal={true}
+                >
+                    {programs.map((program, index) => (
+                        <Pressable
+                            style={[FeedStyles.singleProgramContainer, 
+                                {backgroundColor: index % 2 === 0 ? 'blue' : 'darkblue'}]}  
+                            key={program._id}
+                            onPress={() => navigation.navigate('Program',
+                                { program })}
+                        >
+                            <Text style={FeedStyles.programTitle}>
+                                {program.programName}
+                            </Text>
+                        </Pressable>
                     ))}
-                </Text>
+                </ScrollView>
             </View>
         </View>
     );
--- UFIT/src/screens/style.tsx
+++ UFIT/src/screens/style.tsx
@@ -107,3 +107,44 @@ export const creatingStyles = StyleSheet.create({
     },
 });
 
+export const FeedStyles = StyleSheet.create({
+    viewContainer: {
+        paddingTop: 100,
+        height: '100%',
+    },
+    programsContainer: {
+        width: '100%',
+        marginTop: 20,
+        paddingRight: 40,
+        paddingLeft: 40,
+
+    },
+    singleProgramContainer: {
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+        height: 180,
+        marginTop: 20,
+        borderRadius: 10,
+        paddingLeft: 10,
+    },
+    buttonContainer: {
+        width: '100%',
+        backgroundColor: 'white',
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    titleBarText: {
+        paddingLeft: 20,
+        color: 'gray',
+        fontSize: 30,
+        fontWeight: 'bold',
+    },
+    programTitle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'white',
+        padding: 5,
+    },
+});
+
