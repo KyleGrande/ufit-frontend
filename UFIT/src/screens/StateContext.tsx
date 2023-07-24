ca4705d3911a0e98bfcdd6a50a97ba1be6d3a15f
Ricardo Tlatelpa <ricardotlatelpa@Ricardos-MacBook-Pro.local>
2023-07-23 20:33:44 -0400

    Created Form state context, with yup validation package

--- UFIT/src/screens/StateContext.tsx
+++ UFIT/src/screens/StateContext.tsx
@@ -1,13 +1,36 @@
 import React, {createContext, useContext} from "react";
-import { useForm } from 'react-hook-form';
+import { useForm, UseFormReturn } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
-const FormContext = createContext({});
-
-export const useFormContext = () => useContext(FormContext);
-
-export const FormProvider = ({children}) => {
-    const methods = useForm({
-        defaultValues: {},
-    })
-    return <FormContext.Provider value = {methods}>{children}</FormContext.Provider>
+interface FormData {
+    programName: string;
 }
+
+interface FormContextValue extends UseFormReturn<FormData>{}
+
+const FormContext = createContext<FormContextValue | null>(null);
+
+export const useFormContext = () => {
+    const context = useContext(FormContext);
+    if(!context){
+        throw new Error('useFormContext must be used within a FormProvider');
+    }
+    return context;
+}
+
+const FormProvider: React.FC = ({children}) => {
+    const methods = useForm<FormData>({
+        defaultValues: {},
+        resolver: yupResolver(yup.object().shape({
+            programName: yup.string().required('First name is Required'),
+        })),
+    });    
+      
+    return (
+        <FormContext.Provider value = {methods}>
+            {children}
+        </FormContext.Provider>
+    )    
+}
+export default FormProvider;
