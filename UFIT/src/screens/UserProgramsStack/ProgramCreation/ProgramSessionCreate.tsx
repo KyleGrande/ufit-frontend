839101000340e82b77c5a3268710bb202789826b
Ricardo Tlatelpa <ricardotlatelpa@Ricardos-MacBook-Pro.local>
2023-07-25 23:01:36 -0400

    added session creation form validation and navigation flow

--- UFIT/src/screens/UserProgramsStack/ProgramCreation/ProgramSessionCreate.tsx
+++ UFIT/src/screens/UserProgramsStack/ProgramCreation/ProgramSessionCreate.tsx
@@ -14,15 +14,52 @@ import {
 } from "@react-navigation/native-stack";
 import { StackParamList } from "../../UserPrograms";
 import { Picker } from "@react-native-picker/picker";
+import { useForm, Controller } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 type ProgramsMainScreenProps = {
   navigation: NativeStackNavigationProp<StackParamList, "User Programs">;
 };
 
+interface SessionData {
+  sessionName: string;
+  restDays: string;
+  // movements: [];
+}
+
 export default function ProgramSessionCreate({
   navigation,
 }: ProgramsMainScreenProps) {
-  const [sessionName, onChangeSessionName] = React.useState("");
+  // Form state management + form validation
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SessionData>({
+    defaultValues: {
+      sessionName: "Hello Ricardo",
+      restDays: "3",      
+    },
+    resolver: yupResolver(
+      yup.object().shape({
+        sessionName: yup.string().required("Session name is required"),
+        restDays: yup.string().required("Rest days are required"),
+        // movements: yup
+        //   .array()
+        //   .of(yup.object())
+          // .min(1, "Minimum of 1 movement is required") // test without movements         
+      })
+    ),
+  });
+
+  const onSessionSubmit = (data:any) => {    
+    // use passed by function to store in programs sessions array
+    //navigation.navigate("Create a Program");
+    console.log('Form Data:', data);
+  }
+
+  // const [sessionName, onChangeSessionName] = React.useState("");
   const [restdays, onChangeRestDay] = React.useState("0");
 
   return (
@@ -33,55 +70,81 @@ export default function ProgramSessionCreate({
             Session
           </Text>
           <SafeAreaView style={{ marginTop: 40 }}>
-            <Text style={{ fontSize: 20, color: "white", fontWeight: "bold" }}>
-              Session Name:
-              <Text style={{ flexDirection: "row", color: "red" }}>*</Text>              
-            </Text>
-            <Text style={{ color: "#CECACA", fontSize: 16 }}>
-              (What is this workout session going to be called?)
-            </Text>
-            <TextInput
-              style={{
-                height: 40,
-                margin: 12,
-                borderWidth: 1,
-                padding: 10,
-                color: "white",
-                borderColor: "white",
-                borderRadius: 20
+            <Controller
+              control={control}
+              rules={{
+                required: true,
               }}
-              onChangeText={onChangeSessionName}
-              value={sessionName}
+              name="sessionName"
+              render={({ field, fieldState }) => (
+                <View>
+                  <Text
+                    style={{ fontSize: 20, color: "white", fontWeight: "bold" }}
+                  >
+                    Session Name:
+                    <Text style={{ flexDirection: "row", color: "red" }}>
+                      *
+                    </Text>
+                  </Text>
+                  <Text style={{ color: "#CECACA", fontSize: 16 }}>
+                    (What is this workout session going to be called?)
+                  </Text>
+                  <TextInput
+                    style={{
+                      height: 40,
+                      margin: 12,
+                      borderWidth: 1,
+                      padding: 10,
+                      color: "white",
+                      borderColor: "white",
+                      borderRadius: 20,
+                    }}
+                    onChangeText={field.onChange}
+                    value={field.value}
+                  />
+                </View>
+              )}
             />
-
-            <Text
-              style={{
-                fontSize: 20,
-                color: "white",
-                fontWeight: "bold",
-                marginTop: 40,
-              }}
-            >
-              Rest Days:
-              <Text style={{ flexDirection: "row", color: "red" }}>*</Text>
-            </Text>
-            <Text style={{ color: "#CECACA", fontSize: 16 }}>
-              (The number of rest days after this workout session)
-            </Text>
-            <Picker
-              style={{ color: "white", marginTop: 0, paddingTop: 0 }}
-              selectedValue={restdays}
-              onValueChange={(itemValue: any) => onChangeRestDay(itemValue)}
-              itemStyle={{ color: "white", fontSize: 40 }}
-            >
-              <Picker.Item label="0" value="0" />
-              <Picker.Item label="1" value="1" />
-              <Picker.Item label="2" value="2" />
-              <Picker.Item label="3" value="3" />
-              <Picker.Item label="4" value="4" />
-              <Picker.Item label="5" value="5" />
-              <Picker.Item label="6" value="6" />
-            </Picker>
+          <Controller 
+            control={control}
+            rules={{
+              required: true
+            }}
+            name="restDays"
+            render={({ field, fieldState }) => (
+              <View>
+                <Text
+                style={{
+                  fontSize: 20,
+                  color: "white",
+                  fontWeight: "bold",
+                  marginTop: 40,
+                }}
+              >
+                Rest Days:
+                  <Text style={{ flexDirection: "row", color: "red" }}>*</Text>
+                </Text>
+              <Text style={{ color: "#CECACA", fontSize: 16 }}>
+                (The number of rest days after this workout session)
+              </Text>
+              <Picker
+                style={{ color: "white", marginTop: 0, paddingTop: 0 }}
+                selectedValue={field.value}
+                onValueChange={field.onChange}
+                itemStyle={{ color: "white", fontSize: 40 }}
+              >
+                <Picker.Item label="0" value="0" />
+                <Picker.Item label="1" value="1" />
+                <Picker.Item label="2" value="2" />
+                <Picker.Item label="3" value="3" />
+                <Picker.Item label="4" value="4" />
+                <Picker.Item label="5" value="5" />
+                <Picker.Item label="6" value="6" />
+              </Picker>
+              </View>
+            )}
+          />
+            
           </SafeAreaView>
           <View>
             <Button
@@ -96,10 +159,7 @@ export default function ProgramSessionCreate({
             <Button
               title="Save Session"
               color="orange"
-              onPress={() => {
-                console.log("Creating session");
-                navigation.navigate("Create a Program");
-              }}
+              onPress={handleSubmit(onSessionSubmit)}
             />
           </View>
         </ScrollView>
