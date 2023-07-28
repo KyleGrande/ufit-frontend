2ed997904338f0290cc1be1fd929f50bfb69ce80
Ricardo Tlatelpa <ricardotlatelpa@Ricardos-MacBook-Pro.local>
2023-07-28 13:02:54 -0400

    added comment to movement card option component

--- UFIT/src/screens/UserProgramsStack/components/MovementCardOption.tsx
+++ UFIT/src/screens/UserProgramsStack/components/MovementCardOption.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-
-
-const MovementComponent = ({ movement }:any) => {
+import { View, Text, StyleSheet, TouchableHighlight } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+// TODO: check if package ^ is OK to download
+const MovementComponentOption = ({ movement }:any) => {
     return (
         
             <View style = {{flexDirection: 'row', justifyContent: 'space-between', backgroundColor: 'white', padding: 20, margin: 10, borderRadius: 20}}>
@@ -23,10 +23,36 @@ const MovementComponent = ({ movement }:any) => {
                             <Text>{movement.typeTracking.genMin} Min {movement.typeTracking.genSec} sec</Text>
                         ) : null}
                     </Text>
+                    
+                    <RectButton style={styles.deleteButton} onPress={() => console.log('hello')}>
+                        <Text style={styles.deleteButtonText}>Delete</Text>
+                    </RectButton>
+                    
                 </View>
             </View>
       
     );
   };
+
+  const styles = StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      padding: 16,
+      backgroundColor: '#f0f0f0',
+      borderBottomWidth: 1,
+      borderBottomColor: '#ccc',
+    },
+    deleteButton: {
+      backgroundColor: 'red',
+      paddingVertical: 10,
+      paddingHorizontal: 15,
+      borderRadius: 5,
+    },
+    deleteButtonText: {
+      color: '#fff',
+    },
+  });
   
-  export default MovementComponent;
+  export default MovementComponentOption;
