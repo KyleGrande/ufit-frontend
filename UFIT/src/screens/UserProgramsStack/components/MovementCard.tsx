10513fac5923fa042a97efb02c45a9ec2a92676b
Ricardo Tlatelpa <ricardotlatelpa@Ricardos-MacBook-Pro.local>
2023-07-28 12:34:59 -0400

    key fix on movement feedback component

--- UFIT/src/screens/UserProgramsStack/components/MovementCard.tsx
+++ UFIT/src/screens/UserProgramsStack/components/MovementCard.tsx
@@ -5,7 +5,7 @@ import { View, Text } from 'react-native';
 const MovementComponent = ({ movement }:any) => {
     return (
         
-            <View style = {{flexDirection: 'row', justifyContent: 'space-between', backgroundColor: 'white', width: 300, padding: 20, margin: 10, borderRadius: 20}}>
+            <View style = {{flexDirection: 'row', justifyContent: 'space-between', backgroundColor: 'white', padding: 20, margin: 10, borderRadius: 20}}>
                 <View>
                     <Text>{movement.movementName}</Text>
                     
@@ -14,10 +14,10 @@ const MovementComponent = ({ movement }:any) => {
                 <View>
                     <Text>
                         {movement.typeTracking.trackingType === "setsreps" ? (
-                            <Text>{movement.trackingType.sets} X {movement.trackingType.reps}</Text>
+                            <Text>{movement.typeTracking.sets} X {movement.typeTracking.reps}</Text>
                         ) : null}
                         {movement.typeTracking.trackingType === "rounds" ? (
-                            <Text>Rounds: {movement.trackingType.rounds}</Text>
+                            <Text>Rounds: {movement.typeTracking.rounds}</Text>
                         ) : null}
                         {movement.typeTracking.trackingType === "timer" ? (
                             <Text>{movement.typeTracking.genMin} Min {movement.typeTracking.genSec} sec</Text>
