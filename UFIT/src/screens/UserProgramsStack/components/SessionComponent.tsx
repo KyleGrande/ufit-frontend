408654bff8073f9b31575d77bce9b31036cc8feb
Ricardo Tlatelpa <ricardotlatelpa@Ricardos-MacBook-Pro.local>
2023-07-28 00:05:39 -0400

    edited session component styling

--- UFIT/src/screens/UserProgramsStack/components/SessionComponent.tsx
+++ UFIT/src/screens/UserProgramsStack/components/SessionComponent.tsx
@@ -1,2 +1,29 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import MovementComponent from './MovementCard';
+
+const SessionComponent = ({ session }:any) => {
+    return (
+        <View>
+            <Text 
+            style={{
+                fontSize: 16,
+                color: "white",
+                fontWeight: "bold",
+                marginBottom: 0,
+                paddingBottom: 0,
+            }}
+            >
+                {session.sessionName}                
+            </Text>
+                <View style = {{backgroundColor: "white", borderRadius: 20}}>
+                {session.movements.map((movement:any) => (
+                    <MovementComponent movement = {movement}/>
+                ))}
+            </View>
+
+        </View>
+    );
+};
+
+export default SessionComponent;
