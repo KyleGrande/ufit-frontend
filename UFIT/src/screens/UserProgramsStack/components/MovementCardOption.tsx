dc9ceb22bb3a2751759ccae03654bc99ee6d6481
Ricardo Tlatelpa <rtlatelpa4@gmail.com>
2023-08-25 18:08:51 -0400

    updated styling of add movement component

--- UFIT/src/screens/UserProgramsStack/components/MovementCardOption.tsx
+++ UFIT/src/screens/UserProgramsStack/components/MovementCardOption.tsx
@@ -1,39 +1,38 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, TouchableOpacity } from 'react-native';
 
 // Movement card that shows up on session page, can be deleted on button press, below is the view
 const MovementComponentOption = ({ movement, deleteMovement, idx }:any) => {
-    return (
-        
-            <View style = {{flexDirection: 'row', justifyContent: 'space-between', backgroundColor: 'white', padding: 20, margin: 10, borderRadius: 20}}>
-                <View>
-                    <Text>{movement.movementName}</Text>
-                </View>
-
-                <View>
-                    <Text>
-                        {movement.typeTracking.trackingType === "setsreps" ? (
-                            <Text>{movement.typeTracking.sets} X {movement.typeTracking.reps}</Text>
-                        ) : null}
-                        {movement.typeTracking.trackingType === "rounds" ? (
-                            <Text>Rounds: {movement.typeTracking.rounds}</Text>
-                        ) : null}
-                        {movement.typeTracking.trackingType === "timer" ? (
-                            <Text>{movement.typeTracking.genMin} Min {movement.typeTracking.genSec} sec</Text>
-                        ) : null}
-                    </Text>
-                    
-                    <Button
-                      title="Delete Movement"
-                      color="red"
-                      onPress={() => deleteMovement(idx)}
-                    />
-                    
-                </View>
-            </View>
-      
-    );
-  };
+  return (
+    <View style = {{flexDirection: 'column', justifyContent: 'space-around', backgroundColor: 'white', paddingTop: 20, paddingRight:20, paddingLeft:20, paddingBottom: 5, margin: 10, borderRadius: 20, flexDirection: 'column'}} >
+      <View style = {{flexDirection: 'row', justifyContent: 'space-between'}}>
+          <View style = {{width: '60%'}}>
+              <Text 
+              style = {{textAlign: 'left'}}
+              >{movement.movementName}</Text>
+          </View>
+          <View style = {{width: '40%'}}>
+              <Text style = {{textAlign: 'right'}}>
+                  {movement.typeTracking.trackingType === "setsreps" ? (
+                      <Text>{movement.typeTracking.sets} X {movement.typeTracking.reps}</Text>
+                  ) : null}
+                  {movement.typeTracking.trackingType === "rounds" ? (
+                      <Text>Rounds: {movement.typeTracking.rounds}</Text>
+                  ) : null}
+                  {movement.typeTracking.trackingType === "timer" ? (
+                      <Text>{movement.typeTracking.genMin} Min {movement.typeTracking.genSec} sec</Text>
+                  ) : null}
+              </Text>
+          </View>
+      </View>
+            <Button
+              title="Delete"
+              color="red"
+              onPress={() => deleteMovement(idx)}
+            />  
+    </View>
+  );
+};
 
   const styles = StyleSheet.create({
     container: {
